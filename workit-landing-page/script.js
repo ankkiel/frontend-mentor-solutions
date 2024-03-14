57394a6fa95feba2e4d1ddbffe32c1d35b929c51
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2024-03-14 14:48:56 +0300

    fix: fix animation duration for elements

--- workit-landing-page/script.js
+++ workit-landing-page/script.js
@@ -1,52 +1,48 @@
 let btnAnimation = () => {
-    const tlMessageBtn = gsap.timeline({details: {duration: .5}, repeat: -1, repeatDelay: 3, ease: 'power1.inOut'});
-    tlMessageBtn.to('.intro__link', {scale: 2, transformOrigin: "center bottom"})
-        .to('.intro__link', {scale: 1, ease: "power1.out", y: 5});
-    return tlMessageBtn;
+  const tlMessageBtn = gsap.timeline({details: {duration: .5}, repeat: -1, repeatDelay: 3, ease: 'power1.inOut'});
+  tlMessageBtn.to('.intro__link', {scale: 2, transformOrigin: "center bottom"})
+      .to('.intro__link', {scale: 1, ease: "power1.out", y: 5});
+  return tlMessageBtn;
 }
 
 const tl = gsap.timeline({details: {duration: 1.5}});
-tl.fromTo('.intro__heading', {opacity: 0, ease: 'power1.out'}, {y: 0, opacity: 1, ease: 'power1.out'}) 
-  .add(btnAnimation());
+tl.fromTo('.intro__heading', {opacity: 0, ease: 'power1.out'}, {y: 0, opacity: 1, ease: 'power1.out', duration: 2, stagger: 0.5}) 
+.add(btnAnimation());
 
 const tlImage = gsap.timeline({details: {duration: .1}, ease: 'power1.inOut'});
-tlImage.fromTo('.intro__image', {y: 100, opacity: 0}, {y: 0, opacity: 1});
+tlImage.fromTo('.intro__image', {y: 100, opacity: 0}, {y: 0, opacity: 1, duration: 1.5});
 
 
 const featuresItems = Array.from(document.querySelectorAll(".features__item"));
 let tlFeatures = gsap.timeline({paused: true});
 
 featuresItems.forEach((elem, i) => {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: "top 50%",
-    animation: tlFeatures.from(elem, {
-      autoAlpha: 0,
-      y: 150,
-      duration: 1.5,
-      ease: "power1.out"
-    }, i * .3)
-  });
+ScrollTrigger.create({
+  trigger: elem,
+  start: "top 50%",
+  animation: tlFeatures.from(elem, {
+    autoAlpha: 0,
+    y: 150,
+    duration: 1.5,
+    ease: "power1.out"
+  }, i * .3)
+});
 });
 
 let tlCta = gsap.timeline({paused: true});
 
 tlCta.fromTo('.cta__image-fonder', {
-    autoAlpha: 0,
-    y: 150,
+  autoAlpha: 0,
+  y: 150,
 }, {
-    autoAlpha: 1,
-    y: 0,
-    duration: 1.5,
-    ease: "power1.out"
+  autoAlpha: 1,
+  y: 0,
+  duration: 1.5,
+  ease: "power1.out"
 }, 0);
 
 ScrollTrigger.create({
-    trigger: '.cta',
-    start: "top 65%",
-    animation: tlCta
+  trigger: '.cta',
+  start: "top 65%",
+  animation: tlCta
 });
-
-
-
-
